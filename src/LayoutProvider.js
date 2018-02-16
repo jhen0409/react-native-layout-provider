8da37786e2f64c90379f22d4246ff86bf9c06896
Christopher Bero <chris@firia.com>
2018-02-16 14:20:21 -0600

    Import PropTypes from 'prop-types'

--- src/LayoutProvider.js
+++ src/LayoutProvider.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes, Children } from 'react'
+import { Component, Children } from 'react'
+import { PropTypes } from 'prop-types'
 import { Dimensions } from 'react-native'
 import shallowEqual from 'shallowequal'
 
