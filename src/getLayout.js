6d17de8e3b5a5233aee39f6e0f11a679ece00e8a
Christopher <bigbero@gmail.com>
2018-02-23 01:07:59 -0600

    Import PropTypes from 'prop-types' (#1)
    * Import PropTypes from 'prop-types'
    
    * Switch gitLayout.js to 'prop-types' as well.

--- src/getLayout.js
+++ src/getLayout.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes, createElement } from 'react'
+import { Component, createElement } from 'react'
+import { PropTypes } from 'prop-types'
 import shallowEqual from 'shallowequal'
 import hoistStatics from 'hoist-non-react-statics'
 
